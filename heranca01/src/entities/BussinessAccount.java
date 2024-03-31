f10a36d54aa92efab0db2e8d3ea90ea178a3d4d9
abrxu <abrxu@yahoo.com>
2024-03-30 21:29:48 -0300

    started java inheritance classes

---
+++ heranca01/src/entities/BussinessAccount.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class BussinessAccount extends Account {
+	
+	private Double loanLimit;
+	
+	public BussinessAccount() {
+		super();
+	}
+	
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+
+	public BussinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(Double amount) {
+		if (amount <= loanLimit) {
+			balance += amount - 10.0;
+		}
+	}
+	
+}
