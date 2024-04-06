1d3cd0e3cb03153cf7975b2f3f0d5e9e215fb123
abrxu <abrxu@yahoo.com>
2024-04-06 18:46:34 -0300

    added project files with intelliJ

--- exercicioPropostoFinal2/src/application/Program.java
+++ exercicioPropostoFinal2/src/application/Program.java
@@ -5,7 +5,7 @@ import entities.Company;
 import entities.Individual;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 import java.util.Locale;
 import java.util.Scanner;
 
